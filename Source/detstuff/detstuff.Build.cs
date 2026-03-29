3e0199cb325561ba9e42172c633e04dba0dd6af8
Dat Le Duc <leducdat0907@gmail.com>
2026-03-29 16:00:25 +0700

    add public dependency AIModule

--- Source/detstuff/detstuff.Build.cs
+++ Source/detstuff/detstuff.Build.cs
@@ -8,7 +8,15 @@ public class detstuff : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] 
+		{ 
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
