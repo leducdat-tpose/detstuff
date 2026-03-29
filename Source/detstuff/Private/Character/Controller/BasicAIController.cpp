5095193f9003e69eaba5e125e10ede0435386104
Dat Le Duc <leducdat0907@gmail.com>
2026-03-29 16:08:46 +0700

    add base ai controller class

---
+++ Source/detstuff/Private/Character/Controller/BasicAIController.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "detstuff/Public/Character/Controller/BasicAIController.h"
+
+
+// Sets default values
+ABasicAIController::ABasicAIController(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABasicAIController::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABasicAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/detstuff/Public/Character/Controller/BasicAIController.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Runtime/AIModule/Classes/AIController.h"
+#include "BasicAIController.generated.h"
+
+UCLASS()
+class DETSTUFF_API ABasicAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	ABasicAIController(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
