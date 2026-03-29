4eabc74ab540894e7616e608bd76bff20381b5dc
Dat Le Duc <leducdat0907@gmail.com>
2026-03-29 16:08:36 +0700

    add base ai character class

---
+++ Source/detstuff/Private/Character/BasicAICharacter.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "detstuff/Public/Character/BasicAICharacter.h"
+
+ABasicAICharacter::ABasicAICharacter(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABasicAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ABasicAICharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/detstuff/Public/Character/BasicAICharacter.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasicCharacter.h"
+#include "BasicAICharacter.generated.h"
+
+UCLASS(Blueprintable)
+class DETSTUFF_API ABasicAICharacter : public ABasicCharacter
+{
+	GENERATED_BODY()
+
+public:
+	ABasicAICharacter(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
