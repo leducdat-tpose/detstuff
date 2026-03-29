ff7096d27be43ec91df0a99daf43d875505284e8
Dat Le Duc <leducdat0907@gmail.com>
2026-03-29 16:08:16 +0700

    add base character class

---
+++ Source/detstuff/Private/Character/BasicCharacter.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "detstuff/Public/Character/BasicCharacter.h"
+
+ABasicCharacter::ABasicCharacter(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABasicCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABasicCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABasicCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
